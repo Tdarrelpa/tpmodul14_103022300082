e2b4c5624d94eb4ecf423d67b62c1c9f292b5c5a
Tdarrelpa <darrel2208@gmail.com>
2025-04-13 16:56:01 +0700

    menyelesaikan program dengan runtime configuration

--- Program.cs
+++ Program.cs
@@ -1,2 +1,57 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using cobaConfig;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var config = new CovidConfig_103022300082();
+        config.LoadConfig();
+
+        Console.WriteLine("Selamat datang di aplikasi pemeriksaan Covid-19");
+        Console.WriteLine($"Satuan suhu saat ini: {config.Dapat_Satuan_suhu()}");
+        Console.Write("Apakah anda ingin mengubah satuan suhu (Y/N) : ");
+        if (Console.ReadLine().ToLower() == "y")
+        {
+            // Untuk mengantisipasi apabila nilai default dari variabel Satuan_Suhu adalah fahrenheit
+            if (config.Dapat_Satuan_suhu() == "celcius") 
+            {
+                config.UbahSatuanSuhuKeFahrenheit();
+            }
+            else 
+            {
+                config.UbahSatuanSuhuKeCelcius();
+            }
+            Console.WriteLine($"Satuan suhu diubah menjadi: {config.Dapat_Satuan_suhu()}");
+        }
+
+        Console.WriteLine("\nPertanyaan");
+        Console.Write($"Berapa suhu badan anda saat ini? (dalam nilai {config.Dapat_Satuan_suhu()}): ");
+        double suhu = Convert.ToDouble(Console.ReadLine());
+
+         bool isValidTemperature = config.Dapat_Satuan_suhu() == "celcius"
+            ? suhu >= 36.5 && suhu <= 37.5
+            : suhu >= 97.7 && suhu <= 99.5;
+
+        // Untuk jarak suhu yang lebih jauh dari batas normal
+        if (!isValidTemperature)
+        {
+            Console.WriteLine($"\n{config.Dapat_Pesan(true)}");
+            return;
+        }
+
+        Console.Write("Berapa hari yang lalu anda terakhir memiliki gejala demam? : ");
+        int hari = Convert.ToInt32(Console.ReadLine());
+        /* Alternatif untuk
+        if (hari <= config.Dapat_Batas_hari_demam())
+        {
+            Console.WriteLine($"\n{config.Dapat_Pesan(true)}");
+        }
+        else
+        {
+            Console.WriteLine($"\n{config.Dapat_Pesan(false)}");
+        }
+        */
+        Console.WriteLine(hari <= config.Dapat_Batas_hari_demam() ? $"\n{config.Dapat_Pesan(true)}" 
+            : $"\n{config.Dapat_Pesan(false)}");
+    }
+}
---
+++ cobaConfig.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace cobaConfig
+{
+    public class Rootobject
+    {
+        [JsonPropertyName("satuan_suhu")]
+        public string Satuan_Suhu { get; set; } = "celcius";
+        [JsonPropertyName("batas_hari_demam")]
+        public int Batas_Hari_Demam { get; set; } = 14;
+        [JsonPropertyName("pesan_ditolak")]
+        public string Pesan_Ditolak { get; set; } = "Anda tidak diperbolehkan masuk ke dalam gedung ini";
+        [JsonPropertyName("pesan_diterima")]
+        public string Pesan_Diterima { get; set; } = "Anda dipersilahkan untuk masuk ke dalam gedung ini";
+    }
+
+    public class CovidConfig_103022300082
+    {
+        private readonly Rootobject objekJSON;
+
+        public static string FilePath = Path.Combine(Directory.GetCurrentDirectory(), "covid_config.json");
+
+        public CovidConfig_103022300082()
+        {
+            objekJSON = new Rootobject();
+        }
+
+        public void UbahSatuanSuhuKeFahrenheit()
+        {
+
+            objekJSON.Satuan_Suhu = objekJSON.Satuan_Suhu == "celcius" ? "fahrenheit" : "celcius";
+            SaveConfig();
+        }
+
+        public void LoadConfig()
+        {
+            if (File.Exists(FilePath))
+            {
+                try
+                {
+                    var configJson = File.ReadAllText(FilePath);
+                    var configFromFile = JsonSerializer.Deserialize<CovidConfig_103022300082>(configJson);
+                    objekJSON.Satuan_Suhu = configFromFile.Dapat_Satuan_suhu();
+                    objekJSON.Batas_Hari_Demam = configFromFile.Dapat_Batas_hari_demam();
+                    objekJSON.Pesan_Ditolak = configFromFile.Dapat_Tolak_Pesanan();
+                    objekJSON.Pesan_Diterima = configFromFile.Dapat_Terima_Pesanan();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Terjadi kesalahan: {ex.Message}");
+                }
+            }
+            else
+            {
+                SaveConfig();
+                Console.WriteLine("File tidak ditemukan, membuat file baru");
+            }
+        }
+
+        public void SaveConfig()
+        {
+            try
+            {
+                var options = new JsonSerializerOptions { WriteIndented = true };
+                var jsonString = JsonSerializer.Serialize(this, options);
+                File.WriteAllText(FilePath, jsonString);
+                Console.WriteLine("Berhasil menyimpan konfigurasi baru");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Gagal menyimpan konfigurasi baru: {ex.Message}");
+            }
+        }
+
+        // 3 metode getter ini dipanggil dalam main, jadi harus dibuat public
+        public string Dapat_Pesan(bool isRejected)
+        {return isRejected ? objekJSON.Pesan_Ditolak : objekJSON.Pesan_Diterima;}
+
+        public string Dapat_Satuan_suhu(){return objekJSON.Satuan_Suhu;}
+        public int Dapat_Batas_hari_demam(){return objekJSON.Batas_Hari_Demam;}
+
+        // 2 metode getter ini tidak dipanggil dalam main, jadi bisa dibuat private
+        private string Dapat_Terima_Pesanan(){return objekJSON.Pesan_Diterima;}
+        private string Dapat_Tolak_Pesanan(){return objekJSON.Pesan_Ditolak;}
+
+        // Antisipasi apabila nilai default dari variabel Satuan_Suhu adalah fahrenheit
+        public void UbahSatuanSuhuKeCelcius()
+        {
+            objekJSON.Satuan_Suhu = objekJSON.Satuan_Suhu == "fahrenheit" ? "celcius" : "fahrenheit";
+            SaveConfig();
+        }
+    }
+}
